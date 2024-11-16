8767c1396cd47786f8dc7e605245daeb4ab56dff
Abhishek-frontend-dev <abhishekarya2968@gmail.com>
2024-11-16 16:53:44 +0530

    maine to

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // React 18+
+import './index.css'; // If you have a global CSS file
+import App from './App'; // Path to your App component
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
